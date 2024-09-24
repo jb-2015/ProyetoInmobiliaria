d4f039f13232cd192c47468d93658d89be290a2f
juan-becerra <juan_matias_becerra@hotmail.com>
2024-09-24 02:26:22 -0300

    Revert "Creación de Rama  Back"

--- Controllers/TipoController.cs
+++ Controllers/TipoController.cs
@@ -14,27 +14,47 @@ public class TipoController: Controller{
         _repo = new RepositorioTipo();
     }
 
-    public IActionResult Index(){
+    // para la vista de detalles del tipo
+    public IActionResult Detalles(int id)
+    {
+        var tipo = _repo.Obtener(id);
+        if (tipo == null)
+        {
+            _logger.LogWarning("No se encontró el tipo con el id: {Id}", id);
+            return NotFound(); // Devuelve un error 404 si el tipo no se encuentra
+        }
+
+        return View(tipo); 
+    }
+
+    public IActionResult Index()
+    {
+        _logger.LogInformation("Se invoca el index.");
         var tipos = _repo.Listar();
         return View(tipos);
     }
 
-    public IActionResult Crear(){
+    public IActionResult Crear()
+    {
         return View();
     }
 
     [HttpPost]
-    public IActionResult Guardar(Tipo tipo){
+    public IActionResult Guardar(Tipo tipo)
+    {
         if (ModelState.IsValid){
             try{
                 if (tipo.IdTipo == 0){
                     _repo.Crear(tipo);
+                    _logger.LogInformation("Se ha creado un nuevo tipo con id: {Id}", tipo.IdTipo);
                 }else{
                     _repo.Modificar(tipo);
+                    _logger.LogInformation("Se ha modificado el tipo con id: {Id}", tipo.IdTipo);
                 }
                 return RedirectToAction("Index");
             }
-            catch (Exception){
+            catch (Exception ex){
+                _logger.LogError(ex, "Ha ocurrido un error al tratar de guardar el tipo con id: {Id}", tipo.IdTipo);
                 ModelState.AddModelError("", "Oops ha ocurrido un error al intentar guardar el tipo");
             }
         }
@@ -42,22 +62,29 @@ public class TipoController: Controller{
     }
 
     [HttpPost]
-    public IActionResult Borrar(int id){
-        try{
+    public IActionResult Borrar(int id)
+    {
+        try
+        {
             _repo.Eliminar(id);
             _logger.LogInformation("Se ha eliminado el tipo con id: {Id}", id);
             return RedirectToAction("Index");
-        }catch (Exception ex){
+        }
+        catch (Exception ex)
+        {
             _logger.LogError(ex, "Error al eliminar el tipo con id: {Id}", id);
             return RedirectToAction("Index");
         }
     }
-    
     public IActionResult Editar(int Id){
-        Tipo tipo = _repo.Obtener(Id);
-        if(tipo != null){
-            return View(tipo);
+        try{
+            if(Id == 0){
+                return View();
+            }else{
+                return View(_repo.Obtener(Id));
+            }
+        }catch(Exception e){
+            return RedirectToAction("Index");
         }
-        return RedirectToAction("Index");
     }
 }
